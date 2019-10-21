fa005269577e8da1860ca6bd850ec57b838e4bce
sayedfullah <42635601+sayedfullah@users.noreply.github.com>
2019-10-21 21:15:18 +0200

    commit three constants

--- xname.js
+++ xname.js
@@ -4,11 +4,18 @@ class Xname
     constructor(app)
     {
         this.app = app;
+        this.canvas;  
+        this.ctx;
+        this.dirSvg = "name.svg";
+        this.x = 60000;
+        this.map = {"B":60123,"C":60062,"F":60040,"H":60091};
+        this.dpi = window.devicePixelRatio; //get DPI
+        this.res = 300;
     }
   
     test()
     {
-        alert("container: " + this.app);
+        alert("container: " + this.res);
     }
 }
  
