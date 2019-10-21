db2d935d28782be16190e89429f2d8b57bd94501
sayedfullah <42635601+sayedfullah@users.noreply.github.com>
2019-10-21 20:55:16 +0200

    second commit

--- xname.js
+++ xname.js
@@ -1,1 +1,14 @@
 function atest(){alert("ates")};
+class Xname
+{
+    constructor(app)
+    {
+        this.app = app;
+    }
+  
+    test()
+    {
+        alert("container: " + this.app);
+    }
+}
+ 
