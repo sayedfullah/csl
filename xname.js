474b632e5438b91d9ee40e9db710e438cc49711b
sayedfullah <42635601+sayedfullah@users.noreply.github.com>
2019-10-22 19:33:43 +0200

    comment const

--- xname.js
+++ xname.js
@@ -3,6 +3,7 @@ class Xname
 {
     constructor(app)
     {
+        /*
         this.app = app;
         this.canvas;  
         this.ctx;
@@ -11,6 +12,7 @@ class Xname
         this.map = {"B":60123,"C":60062,"F":60040,"H":60091};
         this.dpi = window.devicePixelRatio; //get DPI
         this.res = 300;
+        */
     }
   
     test()
