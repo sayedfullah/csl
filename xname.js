5e01b8ee5afe9febab1e0f4d580a5fb41c4689d0
sayedfullah <42635601+sayedfullah@users.noreply.github.com>
2019-10-22 19:39:14 +0200

    debug one

--- xname.js
+++ xname.js
@@ -15,6 +15,7 @@ class Xname
         */
     }
   
+    /*
     test()
     {
         alert("container: " + this.res);
@@ -24,5 +25,6 @@ class Xname
     {
         $(".app").html("<table><tr><td><canvas id='canvas' style='border:1px solid #efefef;width:300px;height:300px' >canvas unsupported!</canvas></td><td><h1 class='ty-product-block-title' style='font-weight:normal'>Classic Name Necklace</h1><div class='ty-product-block__short-description'><p>This name chain necklace is custom made just for you. Select the metal, your name or word, add a motif and motif crystal to make it even more personal.</p></div><div class='ty-control-group ty-product-options__item product-list-field clearfix' ><label for='txt1' class='ty-control-group__label ty-product-options__item-label cm-required'>Name or Word :</label><input type='text' id='txt1' class='ty-valign' maxlength='13' oninput='xn.customText()' /></div><div class='ty-control-group ty-product-options__item product-list-field clearfix'><label for='sel1' class='ty-control-group__label ty-product-options__item-label'>Motif:</label><select id='sel1' onchange='xn.motif()' ><option value='N' selected='selected'>None </option><option value='B'>Butterfly </option><option value='F'>Flower </option><option value='H'>Heart </option></select></div><div class='ty-control-group ty-product-options__item product-list-field clearfix'><label for='sel3' class='ty-control-group__label ty-product-options__item-label'>Metal:</label><select id='sel3' onchange='xn.paintColor()' ><option value='AG'>Silver </option><option value='Y9' selected='selected'>9k Yellow Gold </option><option value='R9'>9k Rose Gold </option><option value='W9'>9k White Gold </option></select></div></td></tr></table>");
     };
+    */
 }
  
